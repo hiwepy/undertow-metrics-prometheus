34950f10da055e76694f335b104c5b82648ea84f
wandl <hiwepy@163.com>
2023-05-02 21:20:16 +0800

    init

--- src/main/java/io/undertow/metrics/UndertowSessionMetrics.java
+++ src/main/java/io/undertow/metrics/UndertowSessionMetrics.java
@@ -1,19 +1,3 @@
-/*
- * Copyright (c) 2019-2029, Dreamlu 卢春梦 (596392912@qq.com & www.dreamlu.net).
- * <p>
- * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE 3.0;
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * <p>
- * http://www.gnu.org/licenses/lgpl.html
- * <p>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package io.undertow.metrics;
 
 import io.micrometer.common.lang.NonNull;
@@ -35,12 +19,12 @@ public class UndertowSessionMetrics extends UndertowMetrics {
 	/**
 	 * session
 	 */
-	private static final String METRIC_NAME_SESSIONS_ACTIVE_MAX 				= UNDERTOW_METRIC_NAME_PREFIX + ".sessions.active.max";
-	private static final String METRIC_NAME_SESSIONS_ACTIVE_CURRENT 			= UNDERTOW_METRIC_NAME_PREFIX + ".sessions.active.current";
-	private static final String METRIC_NAME_SESSIONS_CREATED 					= UNDERTOW_METRIC_NAME_PREFIX + ".sessions.created";
-	private static final String METRIC_NAME_SESSIONS_EXPIRED 					= UNDERTOW_METRIC_NAME_PREFIX + ".sessions.expired";
-	private static final String METRIC_NAME_SESSIONS_REJECTED 					= UNDERTOW_METRIC_NAME_PREFIX + ".sessions.rejected";
-	private static final String METRIC_NAME_SESSIONS_ALIVE_MAX 					= UNDERTOW_METRIC_NAME_PREFIX + ".sessions.alive.max";
+	private static final String METRIC_NAME_SESSIONS_ACTIVE_MAX 				= ".sessions.active.max";
+	private static final String METRIC_NAME_SESSIONS_ACTIVE_CURRENT 			= ".sessions.active.current";
+	private static final String METRIC_NAME_SESSIONS_CREATED 					= ".sessions.created";
+	private static final String METRIC_NAME_SESSIONS_EXPIRED 					= ".sessions.expired";
+	private static final String METRIC_NAME_SESSIONS_REJECTED 					= ".sessions.rejected";
+	private static final String METRIC_NAME_SESSIONS_ALIVE_MAX 					= ".sessions.alive.max";
 
 	public UndertowSessionMetrics(MetricsHandler metricsHandler) {
 		super(metricsHandler);
@@ -54,13 +38,6 @@ public class UndertowSessionMetrics extends UndertowMetrics {
 		super(metricsHandler, namePrefix, tags);
 	}
 
-
-	/**
-	 * Bind metrics to the given registry.
-	 * @param registry
-	 * @param undertow
-	 * @param tags
-	 */
 	@Override
 	public void bindTo(@NonNull MeterRegistry registry, UndertowWebServer undertowWebServer, Undertow undertow, MetricsHandler metricsHandler, String namePrefix, Iterable<Tag> tags){
 		// 如果是 web 监控，添加 session 指标
@@ -79,32 +56,33 @@ public class UndertowSessionMetrics extends UndertowMetrics {
 	 * @param statistics
 	 */
 	private void registerSessionStatistics(MeterRegistry registry, SessionManagerStatistics statistics, String namePrefix, Iterable<Tag> tags) {
-		Gauge.builder(METRIC_NAME_SESSIONS_ACTIVE_MAX, statistics, SessionManagerStatistics::getMaxActiveSessions)
+		
+		Gauge.builder(namePrefix + METRIC_NAME_SESSIONS_ACTIVE_MAX, statistics, SessionManagerStatistics::getMaxActiveSessions)
 				.tags(tags)
 				.baseUnit(BaseUnits.SESSIONS)
 				.register(registry);
 
-		Gauge.builder(METRIC_NAME_SESSIONS_ACTIVE_CURRENT, statistics, SessionManagerStatistics::getActiveSessionCount)
+		Gauge.builder(namePrefix + METRIC_NAME_SESSIONS_ACTIVE_CURRENT, statistics, SessionManagerStatistics::getActiveSessionCount)
 				.tags(tags)
 				.baseUnit(BaseUnits.SESSIONS)
 				.register(registry);
 
-		FunctionCounter.builder(METRIC_NAME_SESSIONS_CREATED, statistics, SessionManagerStatistics::getCreatedSessionCount)
+		FunctionCounter.builder(namePrefix + METRIC_NAME_SESSIONS_CREATED, statistics, SessionManagerStatistics::getCreatedSessionCount)
 				.tags(tags)
 				.baseUnit(BaseUnits.SESSIONS)
 				.register(registry);
 
-		FunctionCounter.builder(METRIC_NAME_SESSIONS_EXPIRED, statistics, SessionManagerStatistics::getExpiredSessionCount)
+		FunctionCounter.builder(namePrefix + METRIC_NAME_SESSIONS_EXPIRED, statistics, SessionManagerStatistics::getExpiredSessionCount)
 				.tags(tags)
 				.baseUnit(BaseUnits.SESSIONS)
 				.register(registry);
 
-		FunctionCounter.builder(METRIC_NAME_SESSIONS_REJECTED, statistics, SessionManagerStatistics::getRejectedSessions)
+		FunctionCounter.builder(namePrefix + METRIC_NAME_SESSIONS_REJECTED, statistics, SessionManagerStatistics::getRejectedSessions)
 				.tags(tags)
 				.baseUnit(BaseUnits.SESSIONS)
 				.register(registry);
 
-		TimeGauge.builder(METRIC_NAME_SESSIONS_ALIVE_MAX, statistics, TimeUnit.SECONDS, SessionManagerStatistics::getHighestSessionCount)
+		TimeGauge.builder(namePrefix + METRIC_NAME_SESSIONS_ALIVE_MAX, statistics, TimeUnit.SECONDS, SessionManagerStatistics::getHighestSessionCount)
 				.tags(tags)
 				.register(registry);
 	}
--- src/main/java/io/undertow/metrics/UndertowXWorkerMetrics.java
+++ src/main/java/io/undertow/metrics/UndertowXWorkerMetrics.java
@@ -1,32 +1,16 @@
-/*
- * Copyright (c) 2019-2029, Dreamlu 卢春梦 (596392912@qq.com & www.dreamlu.net).
- * <p>
- * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE 3.0;
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * <p>
- * http://www.gnu.org/licenses/lgpl.html
- * <p>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package io.undertow.metrics;
 
 import io.micrometer.common.lang.NonNull;
-import io.micrometer.core.instrument.Gauge;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.Tag;
-import io.micrometer.core.instrument.TimeGauge;
 import io.undertow.Undertow;
 import io.undertow.server.handlers.MetricsHandler;
 import org.springframework.boot.web.embedded.undertow.UndertowWebServer;
 import org.xnio.management.XnioWorkerMXBean;
 
-import java.util.concurrent.TimeUnit;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 /**
  * Undertow XWorker Metrics
@@ -57,12 +41,6 @@ public class UndertowXWorkerMetrics extends UndertowMetrics {
 		super(metricsHandler, namePrefix, tags);
 	}
 
-	/**
-	 * Bind metrics to the given registry.
-	 * @param registry
-	 * @param undertow
-	 * @param tags
-	 */
 	@Override
 	public void bindTo(@NonNull MeterRegistry registry, UndertowWebServer undertowWebServer, Undertow undertow, MetricsHandler metricsHandler, String namePrefix, Iterable<Tag> tags){
 
@@ -72,60 +50,25 @@ public class UndertowXWorkerMetrics extends UndertowMetrics {
 		// xWorker 指标
 		registerXWorker(registry, workerMXBean, namePrefix, tags);
 
-
 	};
 
 	private void registerXWorker(MeterRegistry registry, XnioWorkerMXBean workerMXBean, String namePrefix, Iterable<Tag> tags) {
 
-		bindGauge(registry, namePrefix + METRIC_NAME_X_WORK_WORKER_POOL_CORE_SIZE, "XWork core worker pool size", workerMXBean, XnioWorkerMXBean::getCoreWorkerPoolSize, tags);
-		bindGauge(registry, namePrefix + METRIC_NAME_X_WORK_WORKER_POOL_MAX_SIZE, "XWork max worker pool size", workerMXBean, XnioWorkerMXBean::getMaxWorkerPoolSize, tags);
-		bindGauge(registry, namePrefix + METRIC_NAME_X_WORK_WORKER_POOL_SIZE, "XWork worker pool size", workerMXBean, XnioWorkerMXBean::getWorkerPoolSize, tags);
-		bindGauge(registry, namePrefix + METRIC_NAME_X_WORK_WORKER_THREAD_BUSY_COUNT, "XWork busy worker thread count", workerMXBean, XnioWorkerMXBean::getBusyWorkerThreadCount, tags);
-		bindGauge(registry, namePrefix + METRIC_NAME_X_WORK_IO_THREAD_COUNT, "XWork I/O thread count", workerMXBean, XnioWorkerMXBean::getIoThreadCount, tags);
-		bindGauge(registry, namePrefix + METRIC_NAME_X_WORK_WORKER_QUEUE_SIZE, "XWork worker queue size", workerMXBean, XnioWorkerMXBean::getWorkerQueueSize, tags);
-
-
-
-
-		Gauge.builder(namePrefix + METRIC_NAME_X_WORK_WORKER_POOL_CORE_SIZE, workerMXBean, XnioWorkerMXBean::getCoreWorkerPoolSize)
-				.description("XWork core worker pool size")
-				.tags(tags)
-				.tag(METRIC_CATEGORY, workerMXBean.getName())
-				.register(registry);
+		List<Tag> tagsList =  new ArrayList<>();
+		if(Objects.nonNull(tags)){
+			tags.forEach(tag -> tagsList.add(tag));
+		}
+		tagsList.add(Tag.of(METRIC_CATEGORY, workerMXBean.getName()));
 
 		// Number of worker threads. The default is 8 times the number of I/O threads.
-		TimeGauge.builder(namePrefix + METRIC_NAME_X_WORK_WORKER_POOL_CORE_SIZE, workerMXBean, TimeUnit.MILLISECONDS, XnioWorkerMXBean::getCoreWorkerPoolSize)
-				.description("XWork core worker pool size")
-				.tags(tags)
-				.tag(METRIC_CATEGORY, workerMXBean.getName())
-				.register(registry);
+		bindGauge(registry, namePrefix + METRIC_NAME_X_WORK_WORKER_POOL_CORE_SIZE, "XWork core worker pool size", workerMXBean, XnioWorkerMXBean::getCoreWorkerPoolSize, tagsList);
+		bindGauge(registry, namePrefix + METRIC_NAME_X_WORK_WORKER_POOL_MAX_SIZE, "XWork max worker pool size", workerMXBean, XnioWorkerMXBean::getMaxWorkerPoolSize, tagsList);
+		bindGauge(registry, namePrefix + METRIC_NAME_X_WORK_WORKER_POOL_SIZE, "XWork worker pool size", workerMXBean, XnioWorkerMXBean::getWorkerPoolSize, tagsList);
+		bindGauge(registry, namePrefix + METRIC_NAME_X_WORK_WORKER_THREAD_BUSY_COUNT, "XWork busy worker thread count", workerMXBean, XnioWorkerMXBean::getBusyWorkerThreadCount, tagsList);
+		//  Number of I/O threads to create for the worker. The default is the number of available processors.
+		bindGauge(registry, namePrefix + METRIC_NAME_X_WORK_IO_THREAD_COUNT, "XWork I/O thread count", workerMXBean, XnioWorkerMXBean::getIoThreadCount, tagsList);
+		bindGauge(registry, namePrefix + METRIC_NAME_X_WORK_WORKER_QUEUE_SIZE, "XWork worker queue size", workerMXBean, XnioWorkerMXBean::getWorkerQueueSize, tagsList);
 
-		Gauge.builder(namePrefix + METRIC_NAME_X_WORK_WORKER_POOL_MAX_SIZE, workerMXBean, XnioWorkerMXBean::getMaxWorkerPoolSize)
-				.description("XWork max worker pool size")
-				.tags(tags)
-				.tag(METRIC_CATEGORY, workerMXBean.getName())
-				.register(registry);
-		Gauge.builder(namePrefix + METRIC_NAME_X_WORK_WORKER_POOL_SIZE, workerMXBean, XnioWorkerMXBean::getWorkerPoolSize)
-				.description("XWork worker pool size")
-				.tags(tags)
-				.tag(METRIC_CATEGORY, workerMXBean.getName())
-				.register(registry);
-		Gauge.builder(namePrefix + METRIC_NAME_X_WORK_WORKER_THREAD_BUSY_COUNT, workerMXBean, XnioWorkerMXBean::getBusyWorkerThreadCount)
-				.description("XWork busy worker thread count")
-				.tags(tags)
-				.tag(METRIC_CATEGORY, workerMXBean.getName())
-				.register(registry);
-		//  Number of I/O threads to create for the worker.
-		Gauge.builder(namePrefix + METRIC_NAME_X_WORK_IO_THREAD_COUNT, workerMXBean, XnioWorkerMXBean::getIoThreadCount)
-				.description("XWork I/O thread count")
-				.tags(tags)
-				.tag(METRIC_CATEGORY, workerMXBean.getName())
-				.register(registry);
-		Gauge.builder(namePrefix + METRIC_NAME_X_WORK_WORKER_QUEUE_SIZE, workerMXBean, XnioWorkerMXBean::getWorkerQueueSize)
-				.description("XWork worker queue size")
-				.tags(tags)
-				.tag(METRIC_CATEGORY, workerMXBean.getName())
-				.register(registry);
 	}
 
 }
